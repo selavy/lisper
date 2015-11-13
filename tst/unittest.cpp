4c0b90288c3285b77ec22f351c58434d8c7f26fd
Peter Lesslie <pclesslie@gmail.com>
2015-11-12 23:08:21 -0600

    Add CPPUNIT and run tests after compile

---
+++ tst/unittest.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+// #include <cppunit/TestCase.h>
+// #include <cppunit/TestFixture.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+
+
+int main(int argc, char** argv)
+{
+    CPPUNIT_NS::TestResult testresult;
+    CPPUNIT_NS::TestResultCollector collectedresults;
+    testresult.addListener(&collectedresults);
+
+    CPPUNIT_NS::BriefTestProgressListener progress;
+    testresult.addListener(&progress);
+
+    CPPUNIT_NS::TestRunner testrunner;
+    testrunner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+    testrunner.run(testresult);
+
+    CPPUNIT_NS::CompilerOutputter compileroutputter(&collectedresults, std::cerr);
+    compileroutputter.write();
+
+    std::ofstream xmlFileOut("unittest_results.xml");
+    CPPUNIT_NS::XmlOutputter xmlOut(&collectedresults, xmlFileOut);
+    xmlOut.write();
+    
+    return !collectedresults.wasSuccessful();
+}
