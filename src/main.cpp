3abf941beb6ae63d3a3716b54be2e25f9eae0496
Peter Lesslie <pclesslie@gmail.com>
2015-11-28 14:21:11 -0600

    Re-structure main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
 #include <sstream>
+#include <locale>
+#include <cstdlib>
 #include "tokenize.h"
 #include "number.h"
 #include "integer.h"
 #include "abstract_syntax_tree.h"
 
-int main(int argc, char** argv)
+void process(const char* str)
 {
-    std::cout << ">";
     std::stringstream ss;
-    ss << "(+ 1 2)\n";
+    ss << str;
     const auto tokens = tokenize(ss);
     std::cout << "Tokens: " << tokens << std::endl;
 
-    AbstractSyntaxTree ast;
-    for (const auto& token : tokens)
-        ast.addToken(token);
+    for (const auto& token: tokens) {
+        if (token == "(") {
+            std::cout << "BEGIN EXPR\n";
+        }
+        else if (token == ")") {
+            std::cout << "END EXPR\n";
+        }
+        else if (token[0] == '"') {
+            std::cout << "STRING\n";
+        }
+        else if (std::isdigit(token[0])) {
+            std::cout << "NUMBER\n";
+        }
+        else {
+            std::cout << "SYMBOL\n";
+        }
+    }
+}
 
-    std::cout << "AST: " << ast << std::endl;
+int main(int argc, char** argv)
+{
+    process("(+ 1 2)");
+    process("(writeln \"hello world\")");
 
+    // token is either part of:
+    // list, if starts with "("
+    // string, if starts with '"'
+    // number, if starts with digit
+    // else symbol
+    
     return 0;
 }
